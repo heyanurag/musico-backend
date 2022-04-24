f41bb10e0deaf70aa800ec94f6e212b8ee509896
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2022-04-25 01:03:31 +0530

    Feature: moodDetector updated

--- musico/mood_analyser/views.py
+++ musico/mood_analyser/views.py
@@ -1,16 +1,23 @@
-from rest_framework import generics, status, viewsets
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 
 import base64
 
 from .model.recog import class_labels, return_mood
-
+from .helper import getTracksByMood
+from authentication.models import User
 
 # Create your views here.
+
+
 class MoodDetector(generics.GenericAPIView):
+
+    permission_classes = (permissions.IsAuthenticated, )
+
     def post(self, request):
         data = request.data
         mood = "Error try again"
+        tracks = []
 
         image_data = data.get('image')
         if image_data:
@@ -23,5 +30,9 @@ class MoodDetector(generics.GenericAPIView):
             model_op = return_mood(file_name)
             if isinstance(model_op, int):
                 mood = class_labels[int(model_op)]
+                if User.objects.filter(id=request.user.pk):
+                    User.objects.filter(id=request.user.pk).update(mood=str(mood))
 
-        return Response({"mood": mood}, status=status.HTTP_200_OK)
+                tracks = getTracksByMood(mood)
+
+        return Response({"mood": mood, "tracks": tracks}, status=status.HTTP_200_OK)
